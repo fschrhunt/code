166b67a9192ab7d2119cfd8d95d30e175fa7142e
fschrhunt <264049687+fschrhunt@users.noreply.github.com>
2026-09-22 15:00:50 -0500

    refactor: drop clone from the CLI
    Use ordinary `git clone` into repos/. The wrapper only manages worktrees
    now: new, list, remove, root, upgrade.

--- contrib/completions/code.fish
+++ contrib/completions/code.fish
@@ -1,6 +1,6 @@
 # Fish completion for code.
 complete -c code -f
-for command in clone new list remove root upgrade version help
+for command in new list remove root upgrade version help
   complete -c code -n "__fish_use_subcommand" -a $command
 end
 complete -c code -n "__fish_seen_subcommand_from remove" -l force
